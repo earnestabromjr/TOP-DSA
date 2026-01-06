fd289624755960e486020cafbcf3559062ef4493
earnestabromjr <abromearnest@gmail.com>
2026-01-06 11:23:06 -0600

    Added logic for has function
    Will change logic before last commit

--- hashmap.js
+++ hashmap.js
@@ -3,7 +3,15 @@ class HashMap {
 	LOAD_FACTOR = 0.75;
 	CAPACITY = 16;
 
-	hash(key) {}
+	hash(key) {
+		let hashCode = 0;
+
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode += primeNumber * hashCode + key.charCodeAt(i);
+		}
+		return hashCode;
+	}
 
 	set(key, value) {}
 
